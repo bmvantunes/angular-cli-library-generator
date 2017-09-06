a1dac0bd94d469fc6de22a004613dad948b4e57c
Bruno Antunes <bmvantunes@users.noreply.github.com>
2017-09-06 15:00:56 +0100

    fix(inner-real-component.generator): fix component exports to export (#30)
    close #28

--- src/core/component/generator/inner-real-component.generator.ts
+++ src/core/component/generator/inner-real-component.generator.ts
@@ -20,7 +20,7 @@ export class InnerRealComponentGenerator {
     const componentPath = paths.real.folderFullPath;
     const moduleFullPath = paths.real.moduleFullPath;
 
-    const params = ['g', 'c', componentPath, '-m', moduleFullPath, '--exports', 'true', '-cd', 'OnPush'];
+    const params = ['g', 'c', componentPath, '-m', moduleFullPath, '--export', 'true', '-cd', 'OnPush'];
     return await AngularCliWrapper.run(params);
   }
 
